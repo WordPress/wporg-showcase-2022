f65160130ea58b2938f44a1a9cfe9f7e42a9d565
dufresnesteven <steve.dufresne@automattic.com>
2022-10-07 15:04:25 +0900

    Remove the hello world post.

--- env/setup.sh
+++ env/setup.sh
@@ -10,5 +10,8 @@ wp rewrite flush --hard
 wp option update blogname "The WordPress Showcase"
 wp option update blogdescription "The Best WordPress Sites in the World"
 
+# Delete hello world post since we use posts for sites
+wp post delete 1 --force
+
 wp import "${root}/env/showcase-posts.xml" --authors=create
 wp import "${root}/env/showcase-pages.xml" --authors=create
