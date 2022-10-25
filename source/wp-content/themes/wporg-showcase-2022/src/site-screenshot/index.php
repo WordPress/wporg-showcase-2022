a9883beb0795e645697a5baf96db80595dd6222d
dufresnesteven <steve.dufresne@automattic.com>
2022-10-25 15:45:10 +0900

    Change to use functions from the functions.php file.

--- source/wp-content/themes/wporg-showcase-2022/src/site-screenshot/index.php
+++ source/wp-content/themes/wporg-showcase-2022/src/site-screenshot/index.php
@@ -8,6 +8,8 @@
 
 namespace WordPressdotorg\Theme\Showcase_2022\Site_Screenshot;
 
+use function WordPressdotorg\Theme\Showcase_2022\site_screenshot_src;
+
 add_action( 'init', __NAMESPACE__ . '\init' );
 
 /**
@@ -39,7 +41,7 @@ function render() {
 	global $post;
 	$width = 865;
 
-	$screenshot = site_screenshot_src( $width );
+	$screenshot = site_screenshot_src( $post, $width );
 	$srcset = $screenshot;
 
 	if ( '' != $width ) {
@@ -48,52 +50,4 @@ function render() {
 	}
 
 	return "<img src='{$screenshot}' srcset='$srcset 2x' alt='" . the_title_attribute( array( 'echo' => false ) ) . "' class='wp-block-wporg-site-screenshot' />";
-}
-
-/**
- * Retrieve the domain from post meta.
- *
- * @param boolean $rem_trail_slash
- * @return string URL
- */
-function get_site_domain( $rem_trail_slash = false ) {
-	global $post;
-
-	$domain = get_post_meta( $post->ID, 'domain', true );
-
-	//remove trailing slash
-	if ( $rem_trail_slash && ( strrpos( $domain, '/' ) == ( strlen( $domain ) - 1 ) ) ) {
-		$domain = substr( $domain, 0, strlen( $domain ) - 1 );
-	}
-
-	$domain = preg_replace( '#^https?://#i', '', $domain );
-
-	return $domain;
-}
-
-/**
- * Returns url of site screenshot image.
- *
- * @param string $width Desired width of screenshot.
- * @return string
- */
-function site_screenshot_src( $width = '' ) {
-	global $post;
-
-	$screenshot = get_post_meta( $post->ID, 'screenshot', true );
-
-	if ( empty( $screenshot ) ) {
-		$screenshot = 'https://wordpress.com/mshots/v1/http%3A%2F%2F' . get_site_domain();
-	} elseif ( function_exists( 'jetpack_photon_url' ) ) {
-		$screenshot = jetpack_photon_url( $screenshot );
-	}
-
-	if ( $width ) {
-		$screenshot = add_query_arg( 'w', $width, $screenshot );
-	}
-
-	$screenshot = apply_filters( 'wporg_showcase_screenshot_src', $screenshot, $post, $width );
-
-	// force screenshot URLs to be https
-	return str_replace( 'http://', 'https://', $screenshot );
 }
