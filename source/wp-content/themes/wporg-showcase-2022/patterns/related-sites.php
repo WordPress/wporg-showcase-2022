1b8fc6057af7f374d00fa19f6ede642a77ee19d5
dufresnesteven <steve.dufresne@automattic.com>
2022-10-25 21:30:44 +0900

    Move related sites to its own pattern.

---
+++ source/wp-content/themes/wporg-showcase-2022/patterns/related-sites.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Related Posts
+ * Slug: wporg-showcase-2022/related-posts
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+	<!-- wp:heading {"level":3} -->
+	<h3><?php _e( 'Related sites', 'wporg' ); ?></h3>
+	<!-- /wp:heading -->
+
+	<!-- wp:jetpack/related-posts {"displayDate":false,"displayThumbnails":true} /-->
+</div>
+<!-- /wp:group -->
