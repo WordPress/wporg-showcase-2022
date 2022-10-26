64f76752925f74b73cf6a21cbda515d8f78a9d35
dufresnesteven <steve.dufresne@automattic.com>
2022-10-26 09:32:21 +0900

    Change _e to esc_attr_e.

--- source/wp-content/themes/wporg-showcase-2022/patterns/related-sites.php
+++ source/wp-content/themes/wporg-showcase-2022/patterns/related-sites.php
@@ -8,7 +8,7 @@
 <!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group">
 	<!-- wp:heading {"level":3} -->
-	<h3><?php _e( 'Related sites', 'wporg' ); ?></h3>
+	<h3><?php esc_attr_e( 'Related sites', 'wporg' ); ?></h3>
 	<!-- /wp:heading -->
 
 	<!-- wp:jetpack/related-posts {"displayDate":false,"displayThumbnails":true} /-->
