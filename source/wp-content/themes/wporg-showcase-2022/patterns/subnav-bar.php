56dd8939848b73bc7b5b363a81a02151115b780d
dufresnesteven <steve.dufresne@automattic.com>
2022-11-01 14:43:18 +0900

    Redo subnav.

--- source/wp-content/themes/wporg-showcase-2022/patterns/subnav-bar.php
+++ source/wp-content/themes/wporg-showcase-2022/patterns/subnav-bar.php
@@ -6,13 +6,12 @@
  */
 ?>
 
-<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"16px","right":"var:preset|spacing|20","bottom":"0","left":"var:preset|spacing|20"}}},"backgroundColor":"white","textColor":"white","className":"is-style-brush-stroke","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull is-style-brush-stroke has-white-color has-white-background-color has-text-color has-background has-link-color" style="padding-top:16px;padding-right:var(--wp--preset--spacing--20);padding-bottom:0;padding-left:var(--wp--preset--spacing--20)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"16px","right":"var:preset|spacing|20","bottom":"0","left":"var:preset|spacing|20"},"margin":{"bottom":"16px"}}},"backgroundColor":"white","textColor":"white","className":"is-style-brush-stroke","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-style-brush-stroke has-white-color has-white-background-color has-text-color has-background has-link-color" style="margin-bottom:16px;padding-top:16px;padding-right:var(--wp--preset--spacing--20);padding-bottom:0;padding-left:var(--wp--preset--spacing--20)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group alignwide"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"textColor":"charcoal-1","fontSize":"normal"} -->
-<p class="has-charcoal-1-color has-text-color has-normal-font-size" style="font-style:normal;font-weight:700"><?php esc_attr_e( 'Showcase', 'wporg' ); ?></p>
+<p class="has-charcoal-1-color has-text-color has-normal-font-size" style="font-style:normal;font-weight:700">Showcase</p>
 <!-- /wp:paragraph -->
 
 <!-- wp:navigation {"ref":7268,"textColor":"charcoal-1","backgroundColor":"white","className":"is-style-dropdown-on-mobile","style":{"spacing":{"blockGap":"var:preset|spacing|30"}}} /--></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
-
