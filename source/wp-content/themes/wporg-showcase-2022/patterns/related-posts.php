ffed877f79cb719c4ce74f3cc43f7548d747eb59
dufresnesteven <steve.dufresne@automattic.com>
2022-11-02 21:58:28 +0900

    Avoid constrained layouts.

--- source/wp-content/themes/wporg-showcase-2022/patterns/related-posts.php
+++ source/wp-content/themes/wporg-showcase-2022/patterns/related-posts.php
@@ -5,7 +5,7 @@
  */
 
 ?>
-<!-- wp:group {"layout":{"type":"constrained"}} -->
+<!-- wp:group -->
 <div class="wp-block-group">
 	<!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"normal","fontFamily":"inter"} -->
 	<h3 class="has-inter-font-family has-normal-font-size" style="font-style:normal;font-weight:700"><?php esc_attr_e( 'Related sites', 'wporg' ); ?></h3>
