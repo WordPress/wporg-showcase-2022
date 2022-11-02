add9c1be2a9e3a1c8f8d6591addda5b07af4c175
dufresnesteven <steve.dufresne@automattic.com>
2022-11-02 21:20:10 +0900

    Shift to jetpack related posts block because of JetPack.
    See: https://github.com/Automattic/jetpack/issues/27200

---
+++ source/wp-content/themes/wporg-showcase-2022/patterns/related-posts.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Related Posts
+ * Slug: wporg-showcase-2022/related-posts
+ */
+
+?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+	<!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"normal","fontFamily":"inter"} -->
+	<h3 class="has-inter-font-family has-normal-font-size" style="font-style:normal;font-weight:700"><?php esc_attr_e( 'Related sites', 'wporg' ); ?></h3>
+	<!-- /wp:heading -->
+
+	<!-- wp:jetpack/related-posts {"displayDate":false,"displayThumbnails":true} /-->
+</div>
+<!-- /wp:group -->
