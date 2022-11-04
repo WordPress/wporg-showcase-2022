ba7280e4ea4f869451b6c7ad7878d7758a716bef
dufresnesteven <steve.dufresne@automattic.com>
2022-11-04 12:47:58 +0900

    Update results bar to a pattern.

--- source/wp-content/themes/wporg-showcase-2022/patterns/results-bar.php
+++ source/wp-content/themes/wporg-showcase-2022/patterns/results-bar.php
@@ -7,12 +7,10 @@
 
 ?>
 
-<!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}}} -->
-<div class="wp-block-columns are-vertically-aligned-center" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:column {"verticalAlignment":"center","width":""} -->
-<div class="wp-block-column is-vertically-aligned-center"><!-- wp:wporg/archive-results-context /--></div>
-<!-- /wp:column -->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"className":"wporg-results-bar-container","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group wporg-results-bar-container" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)">
+<!-- wp:wporg/archive-results-context /-->
 
-<!-- wp:column {"verticalAlignment":"center","width":"200px"} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:200px"><!-- wp:search {"showLabel":false,"placeholder":"Search sites...","width":100,"widthUnit":"%","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true,"className":"is-style-secondary-search-control"} /--></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns -->
+	<!-- wp:search {"showLabel":false,"placeholder":"<?php esc_attr_e( 'Search sites...', 'wporg' ); ?>","width":100,"widthUnit":"%","buttonText":"<?php esc_attr_e( 'Search', 'wporg' ); ?>","buttonPosition":"button-inside","buttonUseIcon":true,"className":"is-style-secondary-search-control"} /-->
+</div>
+<!-- /wp:group -->
