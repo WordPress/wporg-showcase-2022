9c47cc2bf519b5d7e44a87b6bfd94755bb22ed67
dufresnesteven <steve.dufresne@automattic.com>
2022-11-10 11:36:54 +0900

    Replace hardcoded text.

--- source/wp-content/themes/wporg-showcase-2022/patterns/archive-content.php
+++ source/wp-content/themes/wporg-showcase-2022/patterns/archive-content.php
@@ -34,7 +34,7 @@
 	
 	<!-- wp:query-no-results -->
 	<!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results."} -->
-	<p>No results.</p>
+	<p><?php esc_attr_e( 'No results.', 'wporg' ); ?></p>
 	<!-- /wp:paragraph -->
 	<!-- /wp:query-no-results --></div>
 	<!-- /wp:query --></div>
