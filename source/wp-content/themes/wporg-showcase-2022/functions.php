22f00263324fe52855db6ee38fe5def8c00ee42c
dufresnesteven <steve.dufresne@automattic.com>
2022-11-11 09:11:55 +0900

    Add svg img and functions that allows it.

--- source/wp-content/themes/wporg-showcase-2022/functions.php
+++ source/wp-content/themes/wporg-showcase-2022/functions.php
@@ -17,6 +17,7 @@ add_action( 'wp', __NAMESPACE__ . '\jetpackme_remove_rp', 20 );
 add_filter( 'excerpt_length', __NAMESPACE__ . '\modify_excerpt_length', 999 );
 add_filter( 'excerpt_more', __NAMESPACE__ . '\modify_excerpt_more' );
 add_filter( 'query_loop_block_query_vars', __NAMESPACE__ . '\modify_query_loop_block_query_vars', 10, 2 );
+add_filter( 'upload_mimes', __NAMESPACE__ . '\wporg_add_svg_to_upload_mimes' );
 
 /**
  * Enqueue scripts and styles.
@@ -171,4 +172,21 @@ function modify_query_loop_block_query_vars( $query, $block ) {
 	}
 
 	return $query;
+}
+
+/**
+ * Add SVGs to the list of allowed mime types.
+ *
+ * @param array $upload_mimes The existing allowed mime types.
+ *
+ * @return array The enhanced allowed mime types.
+ */
+function wporg_add_svg_to_upload_mimes( $upload_mimes ) {
+	if ( current_user_can( 'manage_options' ) ) {
+		$file_types = array();
+		$file_types['svg'] = 'image/svg+xml';
+		return array_merge( $file_types, $upload_mimes );
+	}
+
+	return $upload_mimes;
 }
