527fcb2d78e4071ea04f69bf9a1bb1013c2bdbb8
dufresnesteven <steve.dufresne@automattic.com>
2022-11-15 08:57:32 +0900

    Localize text.

--- source/wp-content/themes/wporg-showcase-2022/patterns/page-banners.php
+++ source/wp-content/themes/wporg-showcase-2022/patterns/page-banners.php
@@ -8,27 +8,27 @@
 
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|70"}}},"layout":{"type":"default"}} -->
 <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--70)"><!-- wp:paragraph {"align":"center","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|40"}}},"className":"is-style-serif","fontSize":"heading-3"} -->
-<p class="has-text-align-center is-style-serif has-heading-3-font-size" style="padding-bottom:var(--wp--preset--spacing--40)">If your site is featured in the WordPress.org Showcase, you can proudly display these badges on your website.</p>
+<p class="has-text-align-center is-style-serif has-heading-3-font-size" style="padding-bottom:var(--wp--preset--spacing--40)"><?php esc_attr_e( 'If your site is featured in the WordPress.org Showcase, you can proudly display these badges on your website.', 'wporg' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:columns {"verticalAlignment":"center"} -->
 <div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center"} -->
 <div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"align":"center","id":7738,"width":330,"height":84,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image aligncenter size-full is-resized"><img src="http://localhost:8888/wp-content/uploads/2022/11/featured-black-on-white.png" alt="" class="wp-image-7738" width="330" height="84"/></figure>
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="https://s.w.org/images/showcase/featured-black-on-white.png" alt="" class="wp-image-7738" width="330" height="84"/></figure>
 <!-- /wp:image -->
 
 <!-- wp:image {"align":"center","id":7740,"width":337,"height":86,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image aligncenter size-full is-resized"><img src="http://localhost:8888/wp-content/uploads/2022/11/featured-blueberry-on-white.png" alt="" class="wp-image-7740" width="337" height="86"/></figure>
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="https://s.w.org/images/showcase/featured-blueberry-on-white.png" alt="" class="wp-image-7740" width="337" height="86"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"center"} -->
 <div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"align":"center","id":7739,"width":337,"height":86,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image aligncenter size-full is-resized"><img src="http://localhost:8888/wp-content/uploads/2022/11/feature-white-on-black.png" alt="" class="wp-image-7739" width="337" height="86"/></figure>
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="https://s.w.org/images/showcase/featured-white-on-black.png" alt="" class="wp-image-7739" width="337" height="86"/></figure>
 <!-- /wp:image -->
 
 <!-- wp:image {"align":"center","id":7741,"width":337,"height":86,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image aligncenter size-full is-resized"><img src="http://localhost:8888/wp-content/uploads/2022/11/featured-white-on-blueberry.png" alt="" class="wp-image-7741" width="337" height="86"/></figure>
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="https://s.w.org/images/showcase/featured-white-on-blueberry.png" alt="" class="wp-image-7741" width="337" height="86"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
