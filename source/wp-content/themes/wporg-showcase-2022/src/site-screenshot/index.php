115d35cb2c5d671d43f91e3808546cc650eb45a2
dufresnesteven <steve.dufresne@automattic.com>
2022-11-18 08:08:35 +0900

    Remove the screen reader text.

--- source/wp-content/themes/wporg-showcase-2022/src/site-screenshot/index.php
+++ source/wp-content/themes/wporg-showcase-2022/src/site-screenshot/index.php
@@ -54,7 +54,6 @@ function render( $attributes, $content, $block ) {
 	$img_content = "<img src='{$screenshot}' srcset='$srcset 2x' alt='" . the_title_attribute( array( 'echo' => false ) ) . "' />";
 
 	if ( isset( $attributes['isLink'] ) && true == $attributes['isLink'] ) {
-		$img_content .= '<span class="screen-reader-text">' . the_title_attribute( array( 'echo' => false ) ) . '</span>';
 		$img_content = '<a href="' . get_permalink( $post ) . '">' . $img_content . '</a>';
 	}
 
