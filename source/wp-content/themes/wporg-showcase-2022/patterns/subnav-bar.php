5e384a1ae5b2672c5bceda660456853d9ac40552
Kelly Dwan <ryelle@users.noreply.github.com>
2022-11-17 15:43:53 -0500

    Apply style updates from Subpage Header Bar pattern

--- source/wp-content/themes/wporg-showcase-2022/patterns/subnav-bar.php
+++ source/wp-content/themes/wporg-showcase-2022/patterns/subnav-bar.php
@@ -7,12 +7,12 @@
 
 ?>
 
-<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"16px","right":"var:preset|spacing|20","bottom":"0","left":"var:preset|spacing|20"},"margin":{"bottom":"16px"}}},"backgroundColor":"white","textColor":"white","className":"is-style-brush-stroke","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull is-style-brush-stroke has-white-color has-white-background-color has-text-color has-background has-link-color" style="margin-bottom:16px;padding-top:16px;padding-right:var(--wp--preset--spacing--20);padding-bottom:0;padding-left:var(--wp--preset--spacing--20)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
-<div class="wp-block-group alignwide"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"textColor":"charcoal-1","fontSize":"normal"} -->
-<p class="has-charcoal-1-color has-text-color has-normal-font-size" style="font-style:normal;font-weight:700">Showcase</p>
+<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"16px","right":"var:preset|spacing|20","bottom":"0","left":"var:preset|spacing|20"},"margin":{"bottom":"16px"}},"border":{"top":{"color":"var:preset|color|light-grey-1","width":"1px","style":"solid"}}},"backgroundColor":"white","textColor":"white","className":"is-style-brush-stroke is-sticky","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-style-brush-stroke has-white-color has-white-background-color has-text-color has-background has-link-color is-sticky" style="border-top-color:var(--wp--preset--color--light-grey-1);border-top-style:solid;border-top-width:1px;margin-bottom:16px;padding-top:16px;padding-right:var(--wp--preset--spacing--20);padding-bottom:0;padding-left:var(--wp--preset--spacing--20)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"textColor":"charcoal-1","fontSize":"small"} -->
+<p class="has-charcoal-1-color has-text-color has-small-font-size" style="font-weight:700">Showcase</p>
 <!-- /wp:paragraph -->
 
-<!-- wp:navigation {"ref":7299,"textColor":"blueberry-1","backgroundColor":"white","className":"is-style-dropdown-on-mobile","style":{"spacing":{"blockGap":"var:preset|spacing|30"}}} /--></div>
+<!-- wp:navigation {"ref":7299,"textColor":"blueberry-1","backgroundColor":"white","className":"is-style-dropdown-on-mobile","style":{"spacing":{"blockGap":"24px"}},"fontSize":"small"} /--></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
