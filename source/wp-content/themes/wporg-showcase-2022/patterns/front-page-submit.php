d8e9ccb334ff2fe08388ab5761af231a021d0030
dufresnesteven <steve.dufresne@automattic.com>
2022-11-24 14:25:41 +0900

    Remove the constraints.

--- source/wp-content/themes/wporg-showcase-2022/patterns/front-page-submit.php
+++ source/wp-content/themes/wporg-showcase-2022/patterns/front-page-submit.php
@@ -8,7 +8,7 @@
 ?>
 
 <!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"80px","bottom":"80px"}}},"backgroundColor":"charcoal-2","textColor":"white","layout":{"inherit":true,"type":"constrained"}} -->
-<div class="wp-block-group alignfull has-white-color has-charcoal-2-background-color has-text-color has-background has-link-color" id="get-started" style="padding-top:80px;padding-bottom:80px"><!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"60px","padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60"}}},"layout":{"type":"constrained","justifyContent":"left","contentSize":"1160px"}} -->
+<div class="wp-block-group alignfull has-white-color has-charcoal-2-background-color has-text-color has-background has-link-color" id="get-started" style="padding-top:80px;padding-bottom:80px"><!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"60px","padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60"}}},"layout":{"justifyContent":"left"}} -->
 <div class="wp-block-group alignfull" style="padding-right:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"300","letterSpacing":"-2px"}},"className":"is-style-with-arrow","fontSize":"heading-cta","fontFamily":"inter"} -->
 <h2 class="is-style-with-arrow has-inter-font-family has-heading-cta-font-size" style="font-style:normal;font-weight:300;letter-spacing:-2px"><a href="https://wordpress.org/showcase/submit-a-wordpress-site/"><?php esc_attr_e( 'Submit a site', 'wporg' ); ?></a></h2>
 <!-- /wp:heading -->
