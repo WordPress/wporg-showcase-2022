e96ce6ab91a5512b8b95c682ab799b467c6afc4a
dufresnesteven <steve.dufresne@automattic.com>
2022-11-24 12:29:54 +0900

    Revert "Round the images to 2px."
    This reverts commit ab5c0b17f84a45e936dfb0ce264897b97a4b4f80.

--- source/wp-content/themes/wporg-showcase-2022/patterns/archive-content.php
+++ source/wp-content/themes/wporg-showcase-2022/patterns/archive-content.php
@@ -14,8 +14,8 @@
 	<!-- wp:query {"queryId":0,"query":{"inherit":true,"perPage":10},"displayLayout":{"type":"flex","columns":2}} -->
 	<div class="wp-block-query"><!-- wp:post-template -->
 	
-	<!-- wp:group {"style":{"border":{"width":"1px","radius":"2px"}},"borderColor":"light-grey-1"} -->
-	<div class="wp-block-group has-border-color has-light-grey-1-border-color" style="border-width:1px;border-radius:2px"><!-- wp:wporg/site-screenshot {"isLink":true} /--></div>
+	<!-- wp:group {"style":{"border":{"width":"1px"}},"borderColor":"light-grey-1"} -->
+	<div class="wp-block-group has-border-color has-light-grey-1-border-color" style="border-width:1px"><!-- wp:wporg/site-screenshot {"isLink":true} /--></div>
 	<!-- /wp:group -->
 	
 	<!-- wp:post-title {"isLink":true,"fontSize":"normal","fontFamily":"inter"} /-->
