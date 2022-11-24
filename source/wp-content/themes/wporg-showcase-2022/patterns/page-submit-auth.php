45508305e9ec6be2a2312e36fe3f0bcb59f6f6a3
dufresnesteven <steve.dufresne@automattic.com>
2022-11-25 08:59:31 +0900

    Update copy.

--- source/wp-content/themes/wporg-showcase-2022/patterns/page-submit-auth.php
+++ source/wp-content/themes/wporg-showcase-2022/patterns/page-submit-auth.php
@@ -9,7 +9,7 @@
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"0px","padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"}}}}} -->
 <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70)"><!-- wp:heading {"textAlign":"center","style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|20"}}},"fontSize":"heading-3"} -->
-<h2 class="has-text-align-center has-heading-3-font-size" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--20)"><?php esc_attr_e( 'Before you submit...', 'wporg' ); ?></h2>
+<h2 class="has-text-align-center has-heading-3-font-size" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--20)"><?php esc_attr_e( 'Log in to submit a site', 'wporg' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:group {"layout":{"type":"constrained","contentSize":"480px"}} -->
@@ -18,7 +18,7 @@
 <?php
 printf(
 	/* translators: %s is the login url. */
-	wp_kses_post( __( 'Thanks for your interest in submitting a site for the showcase! In order to do so, please <a href="%s">login to your WordPress.org account</a>.', 'wporg' ) ),
+	wp_kses_post( __( 'Thanks for your interest in submitting a site to the Showcase! <a href="%s">Log in to your WordPress.org account</a> to continue.', 'wporg' ) ),
 	esc_url( wp_login_url( home_url( '/' ) . 'submit-a-wordpress-site' ) )
 ); ?></p>
 <!-- /wp:paragraph --></div>
