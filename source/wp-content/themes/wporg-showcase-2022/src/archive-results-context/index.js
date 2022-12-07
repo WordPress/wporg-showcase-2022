29d6046487ea32bd4cff0f23a05426188c80082c
dufresnesteven <steve.dufresne@automattic.com>
2022-12-07 15:04:51 +0900

    Refactor edit.

--- source/wp-content/themes/wporg-showcase-2022/src/archive-results-context/index.js
+++ source/wp-content/themes/wporg-showcase-2022/src/archive-results-context/index.js
@@ -12,45 +12,27 @@ import ServerSideRender from '@wordpress/server-side-render';
  */
 import metadata from './block.json';
 
-/**
- * Render inspector controls.
- *
- * @param {Object}   props                 Component props.
- * @param {string}   props.tagName         The HTML tag name.
- * @param {Function} props.onSelectTagName onChange function for the SelectControl.
- *
- * @return {JSX.Element}                The control group.
- */
-function GroupEditControls( { tagName, onSelectTagName } ) {
-	return (
-		<InspectorControls __experimentalGroup="advanced">
-			<SelectControl
-				label={ __( 'HTML element', 'wporg' ) }
-				options={ [
-					{ label: __( 'Default (<p>)', 'wporg' ), value: 'p' },
-					{ label: '<div>', value: 'div' },
-					{ label: '<h1>', value: 'h1' },
-					{ label: '<h2>', value: 'h2' },
-					{ label: '<h3>', value: 'h3' },
-					{ label: '<h4>', value: 'h4' },
-					{ label: '<h5>', value: 'h5' },
-					{ label: '<h6>', value: 'h6' },
-				] }
-				value={ tagName }
-				onChange={ onSelectTagName }
-			/>
-		</InspectorControls>
-	);
-}
-
 function Edit( { attributes, setAttributes, name } ) {
 	const { tagName } = attributes;
 	return (
 		<>
-			<GroupEditControls
-				tagName={ tagName }
-				onSelectTagName={ ( val ) => setAttributes( { tagName: val } ) }
-			/>
+			<InspectorControls __experimentalGroup="advanced">
+				<SelectControl
+					label={ __( 'HTML element', 'wporg' ) }
+					options={ [
+						{ label: __( 'Default (<p>)', 'wporg' ), value: 'p' },
+						{ label: '<div>', value: 'div' },
+						{ label: '<h1>', value: 'h1' },
+						{ label: '<h2>', value: 'h2' },
+						{ label: '<h3>', value: 'h3' },
+						{ label: '<h4>', value: 'h4' },
+						{ label: '<h5>', value: 'h5' },
+						{ label: '<h6>', value: 'h6' },
+					] }
+					value={ tagName }
+					onChange={ ( val ) => setAttributes( { tagName: val } ) }
+				/>
+			</InspectorControls>
 			<ServerSideRender block={ name } attributes={ attributes } />
 		</>
 	);
