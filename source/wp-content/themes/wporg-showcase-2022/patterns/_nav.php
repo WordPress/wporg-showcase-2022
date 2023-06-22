0b7bac12f711902aef5d1cde043f364e9f13c693
Kelly Dwan <ryelle@users.noreply.github.com>
2023-06-22 18:51:07 -0400

    Update header to use the local navigation block, separate row for breadcrumbs

---
+++ source/wp-content/themes/wporg-showcase-2022/patterns/_nav.php
@@ -0,0 +1,16 @@
+<?php
+// phpcs:disable WordPress.Files.FileName -- Allow underscore for pattern partial.
+/**
+ * Title: Local Nav
+ * Slug: wporg-showcase-2022/nav
+ * Inserter: no
+ *
+ * This nav bar also has the site title, so it should be used on interior pages.
+ */
+
+?>
+<!-- wp:wporg/local-navigation-bar {"backgroundColor":"charcoal-2","style":{"spacing":{"padding":{"right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space","top":"16px","bottom":"16px"}},"position":{"type":"sticky"},"elements":{"link":{"color":{"text":"var:preset|color|white"},":hover":{"color":{"text":"var:preset|color|white"}}}},"border":{"top":{"color":"var:preset|color|charcoal-3","style":"solid","width":"1px"},"right":{},"bottom":{},"left":{}}},"textColor":"white","fontSize":"small"} -->
+<!-- wp:site-title {"level":0,"fontSize":"small"} /-->
+
+<!-- wp:navigation {"ref":7206,"textColor":"white","hasIcon":false,"layout":{"type":"flex","orientation":"horizontal"},"fontSize":"small"} /-->
+<!-- /wp:wporg/local-navigation-bar -->
