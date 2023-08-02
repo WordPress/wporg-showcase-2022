9f8d191f18aff229b45a281642ae82ef4fd1ff63
Kelly Dwan <ryelle@users.noreply.github.com>
2023-08-02 16:36:30 -0400

    Inject the feature color in the header background

--- source/wp-content/themes/wporg-showcase-2022/src/site-screenshot/index.php
+++ source/wp-content/themes/wporg-showcase-2022/src/site-screenshot/index.php
@@ -11,6 +11,7 @@ namespace WordPressdotorg\Theme\Showcase_2022\Site_Screenshot;
 use function WordPressdotorg\Theme\Showcase_2022\get_site_domain;
 
 add_action( 'init', __NAMESPACE__ . '\init' );
+add_filter( 'render_block_core/group', __NAMESPACE__ . '\inject_background_color', 10, 3 );
 
 /**
  * Registers the block using the metadata loaded from the `block.json` file.
@@ -119,4 +120,37 @@ function get_site_screenshot_src( $post, $type = 'desktop' ) {
 	$screenshot_url = apply_filters( 'wporg_showcase_screenshot_src', $screenshot_url, $post );
 
 	return is_ssl() ? set_url_scheme( $screenshot_url, 'https' ) : $screenshot_url;
+}
+
+/**
+ * Filter the group content and inject a background color if we find the background class.
+ *
+ * @param string   $block_content The block content.
+ * @param array    $block         The full block, including name and attributes.
+ * @param WP_Block $instance      The block instance.
+ *
+ * @return string Returns the block markup.
+ */
+function inject_background_color( $block_content, $block, $instance ) {
+	$class = 'has-feature-color-background';
+	if ( ! isset( $block['attrs']['className'] ) || $block['attrs']['className'] !== $class ) {
+		return $block_content;
+	}
+	global $post;
+	$color = get_post_meta( $post->ID, 'feature-color', true );
+	if ( ! $color || '#' === $color ) {
+		return $block_content;
+	}
+
+	// Use the html tag processer to merge with any existing styles.
+	$html = new \WP_HTML_Tag_Processor( $block_content );
+	$html->next_tag();
+	$style = $html->get_attribute( 'style' );
+
+	// Remove existing background if there is one.
+	$style = preg_replace( '/background-color:[^;]+;/', '', $style );
+	$style .= ';background-color:' . esc_attr( $color );
+	$html->set_attribute( 'style', $style );
+
+	return $html->get_updated_html();
 }
