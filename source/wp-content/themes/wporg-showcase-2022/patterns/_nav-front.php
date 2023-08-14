71179e7267ef9cb13283f45fef55ece49b8c3b7c
Kelly Dwan <ryelle@users.noreply.github.com>
2023-08-14 17:37:42 -0400

    Header: Use charcoal-1 for header & local nav background color

--- source/wp-content/themes/wporg-showcase-2022/patterns/_nav-front.php
+++ source/wp-content/themes/wporg-showcase-2022/patterns/_nav-front.php
@@ -8,7 +8,7 @@
  */
 
 ?>
-<!-- wp:wporg/local-navigation-bar {"backgroundColor":"charcoal-2","style":{"spacing":{"padding":{"right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space","top":"16px","bottom":"16px"}},"position":{"type":"sticky"},"elements":{"link":{"color":{"text":"var:preset|color|white"},":hover":{"color":{"text":"var:preset|color|white"}}}},"border":{"top":{"color":"var:preset|color|charcoal-3","style":"solid","width":"1px"},"right":{},"bottom":{},"left":{}}},"textColor":"white","fontSize":"small"} -->
+<!-- wp:wporg/local-navigation-bar {"backgroundColor":"charcoal-1","style":{"spacing":{"padding":{"right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space","top":"16px","bottom":"16px"}},"position":{"type":"sticky"},"elements":{"link":{"color":{"text":"var:preset|color|white"},":hover":{"color":{"text":"var:preset|color|white"}}}},"border":{"top":{"color":"#ffffff0a","style":"solid","width":"1px"},"right":{},"bottom":{},"left":{}}},"textColor":"white","fontSize":"small"} -->
 <!-- wp:spacer {"height":"0"} -->
 <div style="height:0" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
