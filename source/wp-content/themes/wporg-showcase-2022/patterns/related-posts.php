f99d88a8352bdf2f40ce048eae2330ebc963eb48
Kelly Dwan <ryelle@users.noreply.github.com>
2023-09-12 18:55:41 -0400

    Theme: Add Related Posts using a query loop to inherit correct block styles

--- source/wp-content/themes/wporg-showcase-2022/patterns/related-posts.php
+++ source/wp-content/themes/wporg-showcase-2022/patterns/related-posts.php
@@ -4,11 +4,15 @@
  * Slug: wporg-showcase-2022/related-posts
  */
 
+// This pattern loads the Related Posts block, but the markup for the related
+// posts is set with a filter in functions.php.
+// See `jetpack_related_posts_display`.
+
 ?>
-<!-- wp:group -->
-<div class="wp-block-group">
-	<!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"normal","fontFamily":"inter"} -->
-	<h3 class="has-inter-font-family has-normal-font-size" style="font-style:normal;font-weight:700"><?php esc_attr_e( 'Related sites', 'wporg' ); ?></h3>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|edge-space","bottom":"var:preset|spacing|50","left":"var:preset|spacing|edge-space"}}},"backgroundColor":"light-grey-2","layout":{"type":"constrained","wideSize":"1760px"}} -->
+<div class="wp-block-group alignfull has-light-grey-2-background-color has-background" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--edge-space)">
+	<!-- wp:heading {"level":3,"fontSize":"heading-5"} -->
+	<h3 class="wp-block-heading has-heading-5-font-size"><?php esc_html_e( 'Related sites', 'wporg' ); ?></h3>
 	<!-- /wp:heading -->
 
 	<!-- wp:jetpack/related-posts {"displayDate":false,"displayThumbnails":true} /-->
