ff4da566f89acbe950c412e21c78ddc83e5a19d8
Kelly Dwan <ryelle@users.noreply.github.com>
2023-09-14 18:02:15 -0400

    Update content

--- source/wp-content/themes/wporg-showcase-2022/patterns/submit-form.php
+++ source/wp-content/themes/wporg-showcase-2022/patterns/submit-form.php
@@ -7,7 +7,7 @@
 
 ?>
 
-<!-- wp:jetpack/contact-form {"subject":"A new showcase site suggested.","to":"showcase.shouldbeblockedbyfilter@wordpress.org","customThankyou":"redirect","customThankyouRedirect":"<?php echo esc_attr( home_url( '/submit/thanks/' ) ); ?>","style":{"spacing":{"margin":{"top":"var:preset|spacing|20"}}}} -->
+<!-- wp:jetpack/contact-form {"subject":"A new showcase site suggested.","to":"showcase.shouldbeblockedbyfilter@wordpress.org","customThankyou":"redirect","customThankyouRedirect":"<?php echo esc_attr( home_url( '/submit-a-wordpress-site/thanks/' ) ); ?>","style":{"spacing":{"margin":{"top":"var:preset|spacing|20"}}}} -->
 <div class="wp-block-jetpack-contact-form" style="margin-top:var(--wp--preset--spacing--20)">
 	<!-- wp:jetpack/field-name {"label":"Your Name","required":true,"requiredText":"(required)"} /-->
 
@@ -25,9 +25,9 @@
 
 	<!-- wp:jetpack/field-textarea {"label":"Describe the site and, if applicable, the person or organization it represents.","required":true,"requiredText":"(required)"} /-->
 
-	<!-- wp:jetpack/field-textarea {"label":"What justifies this site being added to the WordPress Showcase? what makes it unique or interesting?","required":true,"requiredText":"(required)"} /-->
+	<!-- wp:jetpack/field-textarea {"label":"Why should this site be considered for the WordPress Showcase? What makes it unique or interesting?","required":true,"requiredText":"(required)"} /-->
 
-	<!-- wp:jetpack/field-checkbox {"label":"Check this box if you own the site and agree to be contacted by email","requiredText":"(required)"} /-->
+	<!-- wp:jetpack/field-checkbox {"label":"Check this box if you own the site and agree to be contacted by email."} /-->
 
 	<!-- wp:jetpack/button {"element":"button","text":"Submit site","lock":{"remove":true}} /-->
 </div>
