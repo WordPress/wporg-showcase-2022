ed91834973f73cea379125c18d5ffd65dd7e0c1e
Kelly Dwan <ryelle@users.noreply.github.com>
2023-09-25 18:15:22 -0400

    Make section borders more consistent with opacity values
    Fixes #178

--- source/wp-content/themes/wporg-showcase-2022/patterns/_nav-front.php
+++ source/wp-content/themes/wporg-showcase-2022/patterns/_nav-front.php
@@ -8,7 +8,7 @@
  */
 
 ?>
-<!-- wp:wporg/local-navigation-bar {"backgroundColor":"charcoal-1","style":{"spacing":{"padding":{"right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space","top":"17px","bottom":"18px"}},"position":{"type":"sticky"},"elements":{"link":{"color":{"text":"var:preset|color|white"},":hover":{"color":{"text":"var:preset|color|white"}}}},"border":{"top":{"color":"#ffffff0a","style":"solid","width":"1px"},"right":{},"bottom":{},"left":{}}},"textColor":"white","fontSize":"small"} -->
+<!-- wp:wporg/local-navigation-bar {"backgroundColor":"charcoal-1","style":{"spacing":{"padding":{"right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space","top":"17px","bottom":"18px"}},"position":{"type":"sticky"},"elements":{"link":{"color":{"text":"var:preset|color|white"},":hover":{"color":{"text":"var:preset|color|white"}}}},"border":{"top":{"color":"var:preset|color|white-opacity-15","style":"solid","width":"1px"},"right":{},"bottom":{},"left":{}}},"textColor":"white","fontSize":"small"} -->
 <!-- wp:site-title {"level":1,"fontSize":"small","fontFamily":"inter"} /-->
 
 <!-- wp:navigation {"menuSlug":"showcase","textColor":"white","backgroundColor":"charcoal-1","hasIcon":false,"layout":{"type":"flex","orientation":"horizontal"},"fontSize":"small"} /-->
