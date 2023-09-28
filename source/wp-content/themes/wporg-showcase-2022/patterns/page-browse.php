0084dc2fdbe71fa00f63d6d53d5ed59f42a48102
Nick Diego <nickmdiego@gmail.com>
2023-09-28 15:05:42 -0500

    Browse: Update labels in the browse pattern and remove dots. (#209)
    * Update labels in the browse pattern.
    
    * Remove dots spacer.
    
    * Wrap tags section in group to keep spacing consistent
    
    ---------
    
    Co-authored-by: Kelly Dwan <ryelle@users.noreply.github.com>

--- source/wp-content/themes/wporg-showcase-2022/patterns/page-browse.php
+++ source/wp-content/themes/wporg-showcase-2022/patterns/page-browse.php
@@ -16,8 +16,8 @@
 
 		<!-- wp:group {"layout":{"type":"default"}} -->
 		<div class="wp-block-group">
-			<!-- wp:heading {"level":2,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"normal","fontFamily":"inter"} -->
-			<h2 class="wp-block-heading has-inter-font-family has-normal-font-size" style="font-style:normal;font-weight:400"><?php _e( 'Wonderful WordPress websites by category', 'wporg' ); ?></h2>
+			<!-- wp:heading {"level":2,"style":{"typography":{"fontStyle":"large","fontWeight":"400"}},"fontSize":"normal","fontFamily":"inter"} -->
+			<h2 class="wp-block-heading has-inter-font-family has-large-font-size" style="font-style:normal;font-weight:400"><?php _e( 'Categories', 'wporg' ); ?></h2>
 			<!-- /wp:heading -->
 
 			<!-- wp:wporg/term-grid {"showPostCounts":true} /-->
@@ -26,23 +26,23 @@
 
 		<!-- wp:group {"layout":{"type":"default"}} -->
 		<div class="wp-block-group">
-			<!-- wp:heading {"level":2,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"normal","fontFamily":"inter"} -->
-			<h2 class="wp-block-heading has-inter-font-family has-normal-font-size" style="font-style:normal;font-weight:400"><?php _e( 'Wonderful WordPress websites by flavor', 'wporg' ); ?></h2>
+			<!-- wp:heading {"level":2,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"large","fontFamily":"inter"} -->
+			<h2 class="wp-block-heading has-inter-font-family has-large-font-size" style="font-style:normal;font-weight:400"><?php _e( 'Flavors', 'wporg' ); ?></h2>
 			<!-- /wp:heading -->
 
 			<!-- wp:wporg/term-grid {"showPostCounts":true,"term":"flavor"} /-->
 		</div>
 		<!-- /wp:group -->
 
-		<!-- wp:spacer {"height":"60px","className":"has-dots-background"} -->
-		<div style="height:60px" aria-hidden="true" class="wp-block-spacer has-dots-background"></div>
-		<!-- /wp:spacer -->
+		<!-- wp:group {"layout":{"type":"default"}} -->
+		<div class="wp-block-group">
+			<!-- wp:heading {"level":2,"style":{"typography":{"fontStyle":"large","fontWeight":"400"}},"fontSize":"normal","fontFamily":"inter"} -->
+			<h2 class="wp-block-heading has-inter-font-family has-large-font-size" style="font-style:normal;font-weight:400"><?php _e( 'Tags', 'wporg' ); ?></h2>
+			<!-- /wp:heading -->
 
-		<!-- wp:heading {"level":2,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"normal","fontFamily":"inter"} -->
-		<h2 class="wp-block-heading has-inter-font-family has-normal-font-size" style="font-style:normal;font-weight:400"><?php _e( 'Wonderful WordPress websites by tag', 'wporg' ); ?></h2>
-		<!-- /wp:heading -->
-
-		<!-- wp:wporg/tags-archive /-->
+			<!-- wp:wporg/tags-archive /-->
+		</div>
+		<!-- /wp:group -->
 	</div>
 	<!-- /wp:group -->
 </div>
