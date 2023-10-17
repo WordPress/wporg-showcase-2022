3c2201cc9c054ff7f8fce18fcd1c277a05dea67f
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2023-10-18 08:35:22 +1300

    Use menu icon for local nav

--- source/wp-content/themes/wporg-showcase-2022/patterns/_nav-front.php
+++ source/wp-content/themes/wporg-showcase-2022/patterns/_nav-front.php
@@ -15,5 +15,5 @@
 	</div>
 	<!-- /wp:group -->
 
-	<!-- wp:navigation {"menuSlug":"showcase","textColor":"white","backgroundColor":"charcoal-1","hasIcon":false,"layout":{"type":"flex","orientation":"horizontal"},"style":{"spacing":{"blockGap":"24px"}},"fontSize":"small"} /-->
+	<!-- wp:navigation {"menuSlug":"showcase","textColor":"white","backgroundColor":"charcoal-1","icon":"menu","layout":{"type":"flex","orientation":"horizontal"},"style":{"spacing":{"blockGap":"24px"}},"fontSize":"small"} /-->
 <!-- /wp:wporg/local-navigation-bar -->
